9559724ddcc8063aac1e26cc0574cead9d7cd1b5
Lance Phoenix <108348802+lancepr1@users.noreply.github.com>
2025-05-04 17:46:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+public class Main
+{
+    public static void main(String[]args)
+    {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch game = new WordMatch("aaaabb");
+        System.out.println(game.scoreGuess("a"));
+        System.out.println(game.scoreGuess("aa"));
+        System.out.println(game.scoreGuess("aaa"));
+        System.out.println(game.scoreGuess("aabb"));
+        System.out.println(game.scoreGuess("c"));
+        WordMatch g = new WordMatch("concatenation");
+        System.out.println(g.scoreGuess("ten"));
+        System.out.println(g.scoreGuess("nation"));
+        System.out.println(g.findBetterGuess("ten", "nation"));
+        System.out.println(g.scoreGuess("con"));
+        System.out.println(g.scoreGuess("cat"));
+        System.out.println(g.findBetterGuess("con", "cat"));
 
+    }
+}
